1a4460eef4829e642ceb66d6adadec4cc27a809e
Daniel Esuola <esuoladaniel002@gmail.com>
2022-07-26 01:36:28 +0100

    m

--- src/Components/Travelblog.js
+++ src/Components/Travelblog.js
@@ -7,6 +7,7 @@ import Coffee from '../img/coffee1.jpg';
 import Travel1 from '../img/tr3.jpg';
 import Travel2 from '../img/tr1.jpg';
 import Travel3 from '../img/tr2.jpg';
+import Travel4 from '../img/tr4.jpg';
 import Travel from '../img/travel.jpg';
 
 const Beautyblog = () => {
@@ -14,9 +15,10 @@ const Beautyblog = () => {
         <>
             <Container fluid className='py-5 mt-5'>
                 <Container className='rounded'>
-                    <Card style={{ width: '100%', height: '500px', backgroundImage: `url(${Travel1})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }} className='shadow'>
+                    <Card style={{ width: '100%', height: '500px', backgroundImage: `url(${Travel4})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'top' }} className='shadow'>
                         <Card.ImgOverlay className='d-flex justify-content-center flex-column text-light px-md-5'>
                             <Card.Title className='col-6'><h1 className=' display-1 fw-bold'>Explore.</h1></Card.Title>
+                            <p className="lead col-md-4">Travel the world <br />Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, eligendi!</p>
                         </Card.ImgOverlay>
                     </Card>
                 </Container>
@@ -29,6 +31,7 @@ const Beautyblog = () => {
                                 <Card.Img variant="top" src={Travel2} className='img-fluid rounded h-100' />
                                 <Card.ImgOverlay className='d-flex justify-content-center text-light flex-column px-md-3'>
                                     <Card.Title><h3 className='col-7'>Dubai</h3></Card.Title>
+                                    <p>Lorem ipsum dolor sit amet.</p>
                                     <Card.Link href="#" className='text-light mt-3'>Read More...</Card.Link>
                                 </Card.ImgOverlay>
                             </Card>
@@ -38,6 +41,7 @@ const Beautyblog = () => {
                                 <Card.Img variant="top" src={Travel} className='img-fluid rounded h-100' />
                                 <Card.ImgOverlay className='d-flex justify-content-center flex-column text-light px-md-3'>
                                     <Card.Title><h3 className='col-8'>Santorini</h3></Card.Title>
+                                    <p>Lorem ipsum dolor sit amet.</p>
                                     <Card.Link href="#" className='text-light mt-3'>Read More...</Card.Link>
                                 </Card.ImgOverlay>
                             </Card>
@@ -47,6 +51,7 @@ const Beautyblog = () => {
                                 <Card.Img variant="top" src={Travel3} className='img-fluid rounded h-100' />
                                 <Card.ImgOverlay className='d-flex justify-content-center flex-column text-light px-md-3'>
                                     <Card.Title><h3 className='col-8'>Greece</h3></Card.Title>
+                                    <p>Lorem ipsum dolor sit amet.</p>
                                     <Card.Link href="#" className='text-light mt-3'>Read More...</Card.Link>
                                 </Card.ImgOverlay>
                             </Card>
