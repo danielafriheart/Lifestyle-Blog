8079f1e0f4d730fd1c71d8e6524db397c198570b
Daniel Esuola <esuoladaniel002@gmail.com>
2022-07-31 23:59:54 +0100

    addded morphism to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,6 +4,11 @@ import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 
 function CollapsibleExample() {
+    window.addEventListener('scroll', function () {
+        let header = document.querySelector('nav');
+        let windowPosition = window.scrollY > 0;
+        header.classList.toggle('scrolling-active', windowPosition);
+    });
     return (
         <>
             <Navbar fixed='top' collapseOnSelect expand="lg" bg="" variant="light" className='shadow-sm'>
@@ -12,16 +17,16 @@ function CollapsibleExample() {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mx-auto col-md-6 justify-content-evenly">
-                            <Link to="/" active className='text-secondary activegit '>Home</Link>
-                            <Link to="/Travel" className='text-secondary'>Travel</Link>
-                            <Link to="/Beauty" className='text-secondary'>Beauty</Link>
-                            <Link to="/Food" className='text-secondary'>Food</Link>
-                            <Link to="/Space" className='text-secondary'>Space</Link>
-                            <Link to="/Fashion" className='text-secondary'>Fashion</Link>
-                            <Link to="/Study" className='text-secondary'>Study</Link>
+                            <Link to="/" active className='text-light activegit '>Home</Link>
+                            <Link to="/Travel" className='text-light'>Travel</Link>
+                            <Link to="/Beauty" className='text-light'>Beauty</Link>
+                            <Link to="/Food" className='text-light'>Food</Link>
+                            <Link to="/Space" className='text-light'>Space</Link>
+                            <Link to="/Fashion" className='text-light'>Fashion</Link>
+                            <Link to="/Study" className='text-light'>Study</Link>
                         </Nav>
                         <Nav>
-                            <Link to="#deets" className='text-secondary'>Newsletter</Link>
+                            <Link to="#deets" className='text-light'>Newsletter</Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
